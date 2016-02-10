958be5f7c18f4d99ffcb30fd921b5b59ab907cd6
Steve Gravrock <sgravrock@pivotal.io>
2016-02-10 10:45:34 -0800

    Added test for /api/vi/stops/?{lat, lng, etc}

--- spec/stops_spec.rb
+++ spec/stops_spec.rb
@@ -2,14 +2,21 @@ require 'net/http'
 require 'json'
 require './Config'
 
-describe '/api/v1/stops' do
+def get(uri)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.request(Net::HTTP::Get.new(uri.request_uri))
+end
+
+def get_json(uri)
+  response = get(uri)
+  expect(response.code).to eq('200')
+  expect(response['content-type']).to start_with('application/json')
+  JSON.parse(response.body)
+end
+
+describe '/api/v1/stops/{stop ID}' do
   it 'should return correct JSON for a stop that exists' do
-    uri = URI("#{Config.base_services_url}/api/v1/stops/1_619")
-    http = Net::HTTP.new(uri.host, uri.port)
-    response = http.request(Net::HTTP::Get.new(uri.request_uri))
-    expect(response.code).to eq('200')
-    expect(response['content-type']).to start_with('application/json')
-    data = JSON.parse(response.body)['data']
+    data = get_json(URI("#{Config.base_services_url}/api/v1/stops/1_619"))['data']
     expect(-90..90).to cover(data['latitude'])
     expect(-180..180).to cover(data['longitude'])
     expect(data['stopId']).to eq('1_619')
@@ -26,6 +33,21 @@ describe '/api/v1/stops' do
       expect(departure['temp']).to be_instance_of(Float)
       expect(departure['predictedTime']).to be_instance_of(Fixnum)
       expect(departure['scheduledTime']).to be_instance_of(Fixnum)
+    end
+  end
+end
+
+describe '/api/vi/stops/?{lat, lng, etc}' do
+  it 'should return a list of nearby stops' do
+    data = get_json(URI("#{Config.base_services_url}/api/v1/stops/?lat=47.599&lng=-122.3341&latSpan=0.0172&lngSpan=0.0148"))['data']
+    expect(data).to be_instance_of(Array)
+    # Lots of stops around here, should never be 0
+    expect(data.length).to_not eq(0)
+    data.each do |stop|
+      expect(stop['id']).to be_instance_of(String)
+      expect(stop['name']).to be_instance_of(String)
+      expect(-90..90).to cover(stop['latitude'])
+      expect(-180..180).to cover(stop['longitude'])
     end
   end
 end
